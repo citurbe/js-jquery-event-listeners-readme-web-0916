fca28d49ecdac25b90b16956c9bbba34bb18744a
Craig Iturbe <citurbe@gmail.com>
2016-11-06 09:56:32 -0500

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,29 @@
 //define functions here
+function getIt() {
+  $('p').on('click', function(){alert('Hey!');});
+}
+
+function frameIt() {
+  $('img').on('load', function(){$(this).addClass('tasty')})
+}
+
+function pressIt() {
+  $(document).on('keydown', function(key) {
+    if (key.which === 71){
+      alert('That was a g, G');
+    }
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', function(){alert('Your form is going to be submitted now.')});
+}
 
 $(document).ready(function(){
 
 // call functions here
-
+getIt();
+frameIt();
+pressIt();
+submitIt();
 });
